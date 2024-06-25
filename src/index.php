15895fd4a2c99fe44aa61b624f7ff5f98d99305e
phenphisut <phenphisut.po@github.com>
2024-06-25 07:54:26 +0700

    a

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
- session_start();
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -9,8 +9,7 @@
     <meta charset="utf-8">
     <title>Catster - หน้าแรก</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="" name="keywords">
-    <meta content="" name="description">
+
 
     <!-- Favicon -->
     <link href="img/favicon.ico" rel="icon">
@@ -18,7 +17,7 @@
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600;700&family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">  
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600;700&family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
 
     <!-- Icon Font Stylesheet -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
@@ -30,7 +29,7 @@
     <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Template Stylesheet -->
     <link href="assets/css/style.css" rel="stylesheet">
@@ -41,11 +40,7 @@
     <?php include('include/header.php'); ?>
     <div class="container-xxl bg-white p-0">
         <!-- Spinner Start -->
-        <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-                <span class="sr-only">กำลังโหลด...</span>
-            </div>
-        </div>
+
         <!-- Spinner End -->
 
 
@@ -307,14 +302,14 @@
 
         <script>
             var videoModal = document.getElementById('videoModal');
-            videoModal.addEventListener('show.bs.modal', function (event) {
+            videoModal.addEventListener('show.bs.modal', function(event) {
                 var button = event.relatedTarget;
                 var videoSrc = button.getAttribute('data-src');
                 var iframe = videoModal.querySelector('iframe');
                 iframe.src = videoSrc;
             });
 
-            videoModal.addEventListener('hide.bs.modal', function (event) {
+            videoModal.addEventListener('hide.bs.modal', function(event) {
                 var iframe = videoModal.querySelector('iframe');
                 iframe.src = '';
             });
@@ -480,7 +475,7 @@
             </div>
         </div>
         <!-- Newsletter Start -->
-        
+
 
 
 
