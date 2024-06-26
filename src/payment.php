50ec08e1f936493355cf62b61618b66d7c2dd436
phenphisut <phenphisut.po@github.com>
2024-06-26 23:03:58 +0700

    pps

--- src/payment.php
+++ src/payment.php
@@ -26,17 +26,43 @@ $order = mysqli_fetch_assoc($result);
 
 <!DOCTYPE html>
 <html lang="th">
+
 <head>
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-    <script src="assets/js/bootstrap.min.js"></script>
+    <meta charset="utf-8">
+    <title>Catster - แจ้งชำระเงิน</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600;700&family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="assets/css/style.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/5f1b7c0a83.js" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <title>ชำระเงิน</title>
 </head>
-<body class="bg-body-tertiary">
-    <?php include 'include/menu.php'; ?>
+
+<body style="background-color: #fff;">
+    <?php include 'include/header.php'; ?>
     <div class="container mt-5">
-        <div class="form-structor">
+        <div class="form-structor" style="margin-left: 30%; margin-bottom: 150;">
             <div class="signup">
                 <h2 class="form-title" id="signup"><span>or</span>Qr code</h2>
                 <form action="insert_payment.php" method="POST" enctype="multipart/form-data">
@@ -88,278 +114,283 @@ $order = mysqli_fetch_assoc($result);
             });
         });
     </script>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/tempusdominus/js/moment.min.js"></script>
+    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
 </body>
+
 </html>
 
-
-
-
-
 <style>
-    @import url("https://fonts.googleapis.com/css?family=Fira+Sans");
-
-    html,body {
-    position: relative;
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-family: "Fira Sans", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    }
-
     .form-structor {
-    background-color: #222;
-    border-radius: 15px;
-    height: 550px;
-    width: 450px;
-    position: relative;
-    overflow: hidden;
-    
-    &::after {
-        content: '';
-        opacity: .8;
-        position: absolute;
-        top: 0;right:0;bottom:0;left:0;
-        background-repeat: no-repeat;
-        background-position: left bottom;
-        background-size: 500px;
-        background-image: url('images/stray.jfif');
-    }
-    
-    .signup {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        -webkit-transform: translate(-50%, -50%);
-        width: 65%;
-        z-index: 5;
-        -webkit-transition: all .3s ease;
-        
-        
-        &.slide-up {
-        top: 5%;
-        -webkit-transform: translate(-50%, 0%);
-        -webkit-transition: all .3s ease;
-        }
-        
-        &.slide-up .form-holder,
-        &.slide-up .submit-btn {
-        opacity: 0;
-        visibility: hidden;
-        }
-        
-        &.slide-up .form-title {
-        font-size: 1em;
-        cursor: pointer;
-        }
-        
-        &.slide-up .form-title span {
-        margin-right: 5px;
-        opacity: 1;
-        visibility: visible;
-        -webkit-transition: all .3s ease;
-        }
-        
-        .form-title {
-        color: #fff;
-        font-size: 1.7em;
-        text-align: center;
-        
-        span {
-            color: rgba(0,0,0,0.4);
-            opacity: 0;
-            visibility: hidden;
-            -webkit-transition: all .3s ease;
-        }
-        }
-        
-        .form-holder {
+        background-color: #222;
         border-radius: 15px;
-        background-color: #fff;
+        height: 550px;
+        width: 450px;
+        position: relative;
         overflow: hidden;
-        margin-top: 50px;
-        opacity: 1;
-        visibility: visible;
-        -webkit-transition: all .3s ease;
-        
-        .input {
-            border: 0;
-            outline: none;
-            box-shadow: none;
-            display: block;
-            height: 30px;
-            line-height: 30px;
-            padding: 8px 15px;
-            border-bottom: 1px solid #eee;
-            width: 100%;
-            font-size: 12px;
-            
-            &:last-child {
-            border-bottom: 0;
-            }
-            &::-webkit-input-placeholder {
-            color: rgba(0,0,0,0.4);
-            }
-        }
-        }
-        
-        .submit-btn {
-        background-color: rgba(0,0,0,0.4);
-        color: rgba(256,256,256,0.7);
-        border:0;
-        border-radius: 15px;
-        display: block;
-        margin: 15px auto; 
-        padding: 15px 45px;
-        width: 100%;
-        font-size: 13px;
-        font-weight: bold;
-        cursor: pointer;
-        opacity: 1;
-        visibility: visible;
-        -webkit-transition: all .3s ease;
-        
-        &:hover {
-            transition: all .3s ease;
-            background-color: rgba(0,0,0,0.8);
-        }
-        }
-    }
-    
-    .login {
-        position: absolute;
-        top: 20%;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: #fff;
-        z-index: 5;
-        -webkit-transition: all .3s ease;
-        
-        &::before {
-        content: '';
-        position: absolute;
-        left: 50%;
-        top: -20px;
-        -webkit-transform: translate(-50%, 0);
-        background-color: #fff;
-        width: 200%;
-        height: 250px;
-        border-radius: 50%;
-        z-index: 4;
-        -webkit-transition: all .3s ease;
-        }
-        
-        .center {
-        position: absolute;
-        top: calc(50% - 10%);
-        left: 50%;
-        -webkit-transform: translate(-50%, -50%);
-        width: 65%;
-        z-index: 5;
-        -webkit-transition: all .3s ease;
-        
-        .form-title {
-            color: #000;
-            font-size: 1.7em;
-            text-align: center;
 
-            span {
-            color: rgba(0,0,0,0.4);
-            opacity: 0;
-            visibility: hidden;
+        &::after {
+            content: '';
+            opacity: .8;
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            background-repeat: no-repeat;
+            background-position: left bottom;
+            background-size: 500px;
+            background-image: url('images/stray.jfif');
+        }
+
+        .signup {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            -webkit-transform: translate(-50%, -50%);
+            width: 65%;
+            z-index: 5;
             -webkit-transition: all .3s ease;
+
+
+            &.slide-up {
+                top: 5%;
+                -webkit-transform: translate(-50%, 0%);
+                -webkit-transition: all .3s ease;
+            }
+
+            &.slide-up .form-holder,
+            &.slide-up .submit-btn {
+                opacity: 0;
+                visibility: hidden;
+            }
+
+            &.slide-up .form-title {
+                font-size: 1em;
+                cursor: pointer;
+            }
+
+            &.slide-up .form-title span {
+                margin-right: 5px;
+                opacity: 1;
+                visibility: visible;
+                -webkit-transition: all .3s ease;
+            }
+
+            .form-title {
+                color: #fff;
+                font-size: 1.7em;
+                text-align: center;
+
+                span {
+                    color: rgba(0, 0, 0, 0.4);
+                    opacity: 0;
+                    visibility: hidden;
+                    -webkit-transition: all .3s ease;
+                }
+            }
+
+            .form-holder {
+                border-radius: 15px;
+                background-color: #fff;
+                overflow: hidden;
+                margin-top: 50px;
+                opacity: 1;
+                visibility: visible;
+                -webkit-transition: all .3s ease;
+
+                .input {
+                    border: 0;
+                    outline: none;
+                    box-shadow: none;
+                    display: block;
+                    height: 30px;
+                    line-height: 30px;
+                    padding: 8px 15px;
+                    border-bottom: 1px solid #eee;
+                    width: 100%;
+                    font-size: 12px;
+
+                    &:last-child {
+                        border-bottom: 0;
+                    }
+
+                    &::-webkit-input-placeholder {
+                        color: rgba(0, 0, 0, 0.4);
+                    }
+                }
+            }
+
+            .submit-btn {
+                background-color: rgba(0, 0, 0, 0.4);
+                color: rgba(256, 256, 256, 0.7);
+                border: 0;
+                border-radius: 15px;
+                display: block;
+                margin: 15px auto;
+                padding: 15px 45px;
+                width: 100%;
+                font-size: 13px;
+                font-weight: bold;
+                cursor: pointer;
+                opacity: 1;
+                visibility: visible;
+                -webkit-transition: all .3s ease;
+
+                &:hover {
+                    transition: all .3s ease;
+                    background-color: rgba(0, 0, 0, 0.8);
+                }
             }
         }
 
-        .form-holder {
-            border-radius: 15px;
+        .login {
+            position: absolute;
+            top: 20%;
+            left: 0;
+            right: 0;
+            bottom: 0;
             background-color: #fff;
-            border: 1px solid #eee;
-            overflow: hidden;
-            margin-top: 50px;
-            opacity: 1;
-            visibility: visible;
+            z-index: 5;
             -webkit-transition: all .3s ease;
 
-            .input {
-            border: 0;
-            outline: none;
-            box-shadow: none;
-            display: block;
-            height: 30px;
-            line-height: 30px;
-            padding: 8px 15px;
-            border-bottom: 1px solid #eee;
-            width: 100%;
-            font-size: 12px;
+            &::before {
+                content: '';
+                position: absolute;
+                left: 50%;
+                top: -20px;
+                -webkit-transform: translate(-50%, 0);
+                background-color: #fff;
+                width: 200%;
+                height: 250px;
+                border-radius: 50%;
+                z-index: 4;
+                -webkit-transition: all .3s ease;
+            }
 
-            &:last-child {
-                border-bottom: 0;
-            }
-            &::-webkit-input-placeholder {
-                color: rgba(0,0,0,0.4);
-            }
-            }
-        }
+            .center {
+                position: absolute;
+                top: calc(50% - 10%);
+                left: 50%;
+                -webkit-transform: translate(-50%, -50%);
+                width: 65%;
+                z-index: 5;
+                -webkit-transition: all .3s ease;
 
-        .submit-btn {
-            background-color: #6B92A4;
-            color: rgba(256,256,256,0.7);
-            border:0;
-            border-radius: 15px;
-            display: block;
-            margin: 15px auto; 
-            padding: 15px 45px;
-            width: 100%;
-            font-size: 13px;
-            font-weight: bold;
-            cursor: pointer;
-            opacity: 1;
-            visibility: visible;
-            -webkit-transition: all .3s ease;
+                .form-title {
+                    color: #000;
+                    font-size: 1.7em;
+                    text-align: center;
 
-            &:hover {
-            transition: all .3s ease;
-            background-color: rgba(0,0,0,0.8);
+                    span {
+                        color: rgba(0, 0, 0, 0.4);
+                        opacity: 0;
+                        visibility: hidden;
+                        -webkit-transition: all .3s ease;
+                    }
+                }
+
+                .form-holder {
+                    border-radius: 15px;
+                    background-color: #fff;
+                    border: 1px solid #eee;
+                    overflow: hidden;
+                    margin-top: 50px;
+                    opacity: 1;
+                    visibility: visible;
+                    -webkit-transition: all .3s ease;
+
+                    .input {
+                        border: 0;
+                        outline: none;
+                        box-shadow: none;
+                        display: block;
+                        height: 30px;
+                        line-height: 30px;
+                        padding: 8px 15px;
+                        border-bottom: 1px solid #eee;
+                        width: 100%;
+                        font-size: 12px;
+
+                        &:last-child {
+                            border-bottom: 0;
+                        }
+
+                        &::-webkit-input-placeholder {
+                            color: rgba(0, 0, 0, 0.4);
+                        }
+                    }
+                }
+
+                .submit-btn {
+                    background-color: #6B92A4;
+                    color: rgba(256, 256, 256, 0.7);
+                    border: 0;
+                    border-radius: 15px;
+                    display: block;
+                    margin: 15px auto;
+                    padding: 15px 45px;
+                    width: 100%;
+                    font-size: 13px;
+                    font-weight: bold;
+                    cursor: pointer;
+                    opacity: 1;
+                    visibility: visible;
+                    -webkit-transition: all .3s ease;
+
+                    &:hover {
+                        transition: all .3s ease;
+                        background-color: rgba(0, 0, 0, 0.8);
+                    }
+                }
+            }
+
+            &.slide-up {
+                top: 90%;
+                -webkit-transition: all .3s ease;
+            }
+
+            &.slide-up .center {
+                top: 10%;
+                -webkit-transform: translate(-50%, 0%);
+                -webkit-transition: all .3s ease;
+            }
+
+            &.slide-up .form-holder,
+            &.slide-up .submit-btn {
+                opacity: 0;
+                visibility: hidden;
+                -webkit-transition: all .3s ease;
+            }
+
+            &.slide-up .form-title {
+                font-size: 1em;
+                margin: 0;
+                padding: 0;
+                cursor: pointer;
+                -webkit-transition: all .3s ease;
+            }
+
+            &.slide-up .form-title span {
+                margin-right: 5px;
+                opacity: 1;
+                visibility: visible;
+                -webkit-transition: all .3s ease;
             }
         }
-        }
-        
-        &.slide-up {
-        top: 90%;
-        -webkit-transition: all .3s ease;
-        }
-        
-        &.slide-up .center {
-        top: 10%;
-        -webkit-transform: translate(-50%, 0%);
-        -webkit-transition: all .3s ease;
-        }
-        
-        &.slide-up .form-holder,
-        &.slide-up .submit-btn {
-        opacity: 0;
-        visibility: hidden;
-        -webkit-transition: all .3s ease;
-        }
-        
-        &.slide-up .form-title {
-        font-size: 1em;
-        margin: 0;
-        padding: 0;
-        cursor: pointer;
-        -webkit-transition: all .3s ease;
-        }
-        
-        &.slide-up .form-title span {
-        margin-right: 5px;
-        opacity: 1;
-        visibility: visible;
-        -webkit-transition: all .3s ease;
-        }
-    }
     }
 </style>
