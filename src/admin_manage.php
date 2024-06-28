987897f224334ac78222556970a40f2f9d85f6a0
phenphisut <phenphisut.po@github.com>
2024-06-28 08:54:50 +0700

    pps

--- src/admin_manage.php
+++ src/admin_manage.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,6 +12,7 @@
 
 	<title>AdminHub</title>
 </head>
+
 <body>
 
 
@@ -23,27 +25,45 @@
 		<ul class="side-menu top">
 			<li>
 				<a href="admin.php">
-					<i class='bx bxs-dashboard' ></i>
+					<i class='bx bxs-dashboard'></i>
 					<span class="text">Dashboard</span>
 				</a>
 			</li>
 			<li class="active">
 				<a href="admin_manage.php">
-					<i class='bx bxs-group' ></i>
+					<i class='bx bxs-group'></i>
 					<span class="text">Manage</span>
+				</a>
+			</li>
+			<li>
+				<a href="orders.php">
+					<i class='bx bxs-shopping-bag-alt'></i>
+					<span class="text">Orders</span>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<i class='bx bxs-doughnut-chart'></i>
+					<span class="text">Analytics</span>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<i class='bx bxs-message-dots'></i>
+					<span class="text">Message</span>
 				</a>
 			</li>
 		</ul>
 		<ul class="side-menu">
 			<li>
-				<a href="#">
-					<i class='bx bxs-cog' ></i>
+				<a href="form_edit_profile_admin.php">
+					<i class='bx bxs-cog'></i>
 					<span class="text">Settings</span>
 				</a>
 			</li>
 			<li>
 				<a href="logout.php" class="logout">
-					<i class='bx bxs-log-out-circle' ></i>
+					<i class='bx bxs-log-out-circle'></i>
 					<span class="text">Logout</span>
 				</a>
 			</li>
@@ -57,18 +77,18 @@
 	<section id="content">
 		<!-- NAVBAR -->
 		<nav>
-			<i class='bx bx-menu' ></i>
+			<i class='bx bx-menu'></i>
 			<a href="#" class="nav-link">Manage</a>
 			<form action="#">
 				<div class="form-input">
 					<input type="search" placeholder="Search...">
-					<button type="submit" class="search-btn"><i class='bx bx-search' ></i></button>
+					<button type="submit" class="search-btn"><i class='bx bx-search'></i></button>
 				</div>
 			</form>
 			<input type="checkbox" id="switch-mode" hidden>
 			<label for="switch-mode" class="switch-mode"></label>
 			<a href="#" class="notification">
-				<i class='bx bxs-bell' ></i>
+				<i class='bx bxs-bell'></i>
 				<span class="num">8</span>
 			</a>
 			<a href="#" class="profile">
@@ -83,73 +103,84 @@
 			<div class="head-title">
 				<div class="left">
 					<h1>จัดการข้อมูลพื้นฐาน</h1>
-                    <ul class="breadcrumb">
+					<ul class="breadcrumb">
 						<li>
 							<a href="#">Manage</a>
 						</li>
 					</ul>
 				</div>
 				<a href="#" class="btn-download">
-					<i class='bx bxs-cloud-download' ></i>
+					<i class='bx bxs-cloud-download'></i>
 					<span class="text">Download PDF</span>
 				</a>
 			</div>
 
 			<ul class="box-info">
 				<li>
-					<i class='bx bxs-calendar-check' ></i>
+					<i class='bx bxs-calendar-check'></i>
 					<span class="text">
 						<h3>Catster</h3>
 						<p>บ้านพักพิงแมวจรแคทสเตอร์</p>
 					</span>
 				</li>
-				<a href="employees.php"><li>
-					<i class='bx bxs-group' ></i>
-					<span class="text">
-						<h3>Employees</h3>
-						<p>พนักงาน</p>
-					</span>
-				</li></a>
-				<a href="vaccines.php"><li>
-					<i class='bx bxs-calendar-check' ></i>
-					<span class="text">
-						<h3>Vaccines</h3>
-						<p>วัคซีน</p>
-					</span>
-				</li></a>
+				<a href="employees.php">
+					<li>
+						<i class='bx bxs-group'></i>
+						<span class="text">
+							<h3>Employees</h3>
+							<p>พนักงาน</p>
+						</span>
+					</li>
+				</a>
+				<a href="vaccines.php">
+					<li>
+						<i class='bx bxs-calendar-check'></i>
+						<span class="text">
+							<h3>Vaccines</h3>
+							<p>วัคซีน</p>
+						</span>
+					</li>
+				</a>
 			</ul>
 
-            <ul class="box-info">
-				<a href="types.php"><li>
-					<i class='bx bxs-group' ></i>
-					<span class="text">
-						<h3>Types</h3>
-						<p>ประเภทสินค้า</p>
-					</span>
-				</li></a>
-				<a href="products.php"><li>
-					<i class='bx bxs-dollar-circle' ></i>
-					<span class="text">
-						<h3>Products</h3>
-						<p>สินค้า</p>
-					</span>
-				</li></a>
-				<a href="members.php"><li>
-					<i class='bx bxs-dollar-circle' ></i>
-					<span class="text">
-						<h3>Members</h3>
-						<p>สมาชิก</p>
-					</span>
-				</li></a>
+			<ul class="box-info">
+				<a href="types.php">
+					<li>
+						<i class='bx bxs-group'></i>
+						<span class="text">
+							<h3>Types</h3>
+							<p>ประเภทสินค้า</p>
+						</span>
+					</li>
+				</a>
+				<a href="products.php">
+					<li>
+						<i class='bx bxs-dollar-circle'></i>
+						<span class="text">
+							<h3>Products</h3>
+							<p>สินค้า</p>
+						</span>
+					</li>
+				</a>
+				<a href="members.php">
+					<li>
+						<i class='bx bxs-dollar-circle'></i>
+						<span class="text">
+							<h3>Members</h3>
+							<p>สมาชิก</p>
+						</span>
+					</li>
+				</a>
 			</ul>
 
 		</main>
 		<!-- MAIN -->
 	</section>
 	<!-- CONTENT -->
-	
-	
+
+
 
 	<script src="assets/js/script_admin.js"></script>
 </body>
+
 </html>
